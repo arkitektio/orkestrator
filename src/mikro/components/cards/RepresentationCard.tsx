dd7dd29ab443b598c28f487c9b9c4ce384dddc96
jhnnsrs <jhnnsrs@gmail.com>
2023-05-09 18:22:24 +0200

    fixed truncate

--- src/mikro/components/cards/RepresentationCard.tsx
+++ src/mikro/components/cards/RepresentationCard.tsx
@@ -32,7 +32,7 @@ export const RepresentationCard = ({ rep, mates }: RepresentationCardProps) => {
           blurhash={rep?.latestThumbnail.blurhash}
         />
       )}
-      <div className="px-2 py-2 h-full w-full absolute top-0 left-0 hover:bg-opacity-20 bg-opacity-10 bg-back-999 transition-all ease-in-out duration-200">
+      <div className="px-2 py-2 h-full w-full absolute top-0 left-0 hover:bg-opacity-20 bg-opacity-10 bg-back-999 transition-all ease-in-out duration-200 truncate">
         <Representation.DetailLink
           className={({ isActive } /*  */) =>
             "z-10 font-bold text-md mb-2 cursor-pointer " +
