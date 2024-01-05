3efe33203de17ffa299065fabdaab4cce2681fa8
jhnnsrs <jhnnsrs@gmail.com>
2024-01-05 15:42:23 +0100

    remove colormap

--- src/mikro/components/canvases/TwoDOffcanvas.tsx
+++ src/mikro/components/canvases/TwoDOffcanvas.tsx
@@ -50,16 +50,7 @@ export interface TwoDProps {
   follow?: "width" | "height";
 }
 
-let canvaswidth = 700;
-let canvasheight = 700;
 
-const people = [
-  { id: 1, name: "Durward Reynolds", unavailable: false },
-  { id: 2, name: "Kenton Towne", unavailable: false },
-  { id: 3, name: "Therese Wunsch", unavailable: false },
-  { id: 4, name: "Benedict Kessler", unavailable: true },
-  { id: 5, name: "Katelyn Rohan", unavailable: false },
-];
 
 export const ColorMapSelect = (props: {
   onChange: (colormap: AvailableColormap) => void;
